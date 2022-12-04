1f01872a44cff691f3fd1f1c2e4c3fd79232c4ff
stygian91 <kioravuser@gmail.com>
2022-12-04 09:57:58 +0200

    Day 4 done

---
+++ q_4/src/main.rs
@@ -0,0 +1,60 @@
+use std::{fs::read_to_string, path::Path};
+
+#[derive(Debug)]
+pub struct Section(pub u32, pub u32);
+
+impl Section {
+    pub fn from_str(input: &str) -> Self {
+        let nums: Vec<u32> = input
+            .split('-')
+            .map(|part| part.parse::<u32>().unwrap())
+            .collect();
+
+        Self(nums[0], nums[1])
+    }
+
+    pub fn contains(&self, other: &Section) -> bool {
+        self.0 <= other.0 && self.1 >= other.1
+    }
+
+    pub fn overlaps(&self, other: &Section) -> bool {
+        self.contains(other)
+            || other.contains(self)
+            || (self.1 >= other.0 && self.1 <= other.1)
+            || (self.0 <= other.1 && self.0 >= other.0)
+    }
+}
+
+fn parse_group(line: &str) -> (Section, Section) {
+    let parts = line.split(',').collect::<Vec<&str>>();
+    (Section::from_str(parts[0]), Section::from_str(parts[1]))
+}
+
+fn count_groups<F>(lines: &Vec<&str>, compare: F) -> u32
+where
+    F: Fn(&Section, &Section) -> bool,
+{
+    let mut count: u32 = 0;
+
+    for line in lines {
+        let group = parse_group(line);
+        if compare(&group.0, &group.1) {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn main() {
+    let contents = read_to_string(&Path::new("./data/input.txt")).unwrap();
+    let lines: Vec<&str> = contents.split('\n').collect();
+
+    let part1 = count_groups(&lines, |section1, section2| {
+        section1.contains(section2) || section2.contains(section1)
+    });
+    println!("{}", part1);
+
+    let part2 = count_groups(&lines, |section1, section2| section1.overlaps(section2));
+    println!("{}", part2);
+}
