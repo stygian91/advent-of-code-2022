dcb141f200278d85ae0af4989b7ecfd57f99317c
stygian91 <kioravuser@gmail.com>
2022-12-09 21:59:22 +0200

    Day 9 - part 2.

--- rs/q_9/src/main.rs
+++ rs/q_9/src/main.rs
@@ -13,6 +13,7 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug)]
 struct Move {
     direction: Direction,
     amount: i32,
@@ -22,48 +23,79 @@ type Pos = (i32, i32);
 
 #[derive(Debug)]
 struct Rope {
-    pub head: Pos,
-    pub tail: Pos,
-    pub tail_positions: BTreeSet<Pos>,
+    pub positions: Vec<Pos>,
+    pub visited: BTreeSet<Pos>,
 }
 
 impl Rope {
     pub fn handle_move(&mut self, mv: &Move) {
+        let pos_idx: Vec<usize> = (0..self.positions.len()).collect();
+
         for _ in 0..mv.amount {
-            self.head = match mv.direction {
-                Direction::Up => (self.head.0, self.head.1 + 1),
-                Direction::Down => (self.head.0, self.head.1 - 1),
-                Direction::Left => (self.head.0 - 1, self.head.1),
-                Direction::Right => (self.head.0 + 1, self.head.1),
+            self.positions[0] = match mv.direction {
+                Direction::Up => (self.positions[0].0, self.positions[0].1 + 1),
+                Direction::Down => (self.positions[0].0, self.positions[0].1 - 1),
+                Direction::Left => (self.positions[0].0 - 1, self.positions[0].1),
+                Direction::Right => (self.positions[0].0 + 1, self.positions[0].1),
             };
 
-            if are_neighbours(&self.head, &self.tail) || self.head == self.tail {
-                continue;
+            for win in pos_idx.windows(2) {
+                self.handle_segment_move(win[0], win[1], mv);
             }
 
-            if self.head.0 == self.tail.0 {
-                if self.head.1 > self.tail.1 {
-                    self.tail = (self.tail.0, self.head.1 - 1);
-                } else {
-                    self.tail = (self.tail.0, self.head.1 + 1);
-                }
-            } else if self.head.1 == self.tail.1 {
-                if self.head.0 > self.tail.0 {
-                    self.tail = (self.head.0 - 1, self.tail.1);
-                } else {
-                    self.tail = (self.head.0 + 1, self.tail.1);
-                }
-            } else if self.head.0 < self.tail.0 && self.head.1 > self.tail.1 {
-                self.tail = (self.tail.0 - 1, self.tail.1 + 1);
-            } else if self.head.0 > self.tail.0 && self.head.1 > self.tail.1 {
-                self.tail = (self.tail.0 + 1, self.tail.1 + 1);
-            } else if self.head.0 > self.tail.0 && self.head.1 < self.tail.1 {
-                self.tail = (self.tail.0 + 1, self.tail.1 - 1);
+            self.visited.insert(*self.positions.last().unwrap());
+        }
+    }
+
+    fn handle_segment_move(&mut self, head_idx: usize, tail_idx: usize, mv: &Move) {
+        if are_neighbours(&self.positions[head_idx], &self.positions[tail_idx])
+            || self.positions[head_idx] == self.positions[tail_idx]
+        {
+            return;
+        }
+
+        if self.positions[head_idx].0 == self.positions[tail_idx].0 {
+            if self.positions[head_idx].1 > self.positions[tail_idx].1 {
+                self.positions[tail_idx] =
+                    (self.positions[tail_idx].0, self.positions[head_idx].1 - 1);
             } else {
-                self.tail = (self.tail.0 - 1, self.tail.1 - 1);
+                self.positions[tail_idx] =
+                    (self.positions[tail_idx].0, self.positions[head_idx].1 + 1);
             }
-
-            self.tail_positions.insert(self.tail);
+        } else if self.positions[head_idx].1 == self.positions[tail_idx].1 {
+            if self.positions[head_idx].0 > self.positions[tail_idx].0 {
+                self.positions[tail_idx] =
+                    (self.positions[head_idx].0 - 1, self.positions[tail_idx].1);
+            } else {
+                self.positions[tail_idx] =
+                    (self.positions[head_idx].0 + 1, self.positions[tail_idx].1);
+            }
+        } else if self.positions[head_idx].0 < self.positions[tail_idx].0
+            && self.positions[head_idx].1 > self.positions[tail_idx].1
+        {
+            self.positions[tail_idx] = (
+                self.positions[tail_idx].0 - 1,
+                self.positions[tail_idx].1 + 1,
+            );
+        } else if self.positions[head_idx].0 > self.positions[tail_idx].0
+            && self.positions[head_idx].1 > self.positions[tail_idx].1
+        {
+            self.positions[tail_idx] = (
+                self.positions[tail_idx].0 + 1,
+                self.positions[tail_idx].1 + 1,
+            );
+        } else if self.positions[head_idx].0 > self.positions[tail_idx].0
+            && self.positions[head_idx].1 < self.positions[tail_idx].1
+        {
+            self.positions[tail_idx] = (
+                self.positions[tail_idx].0 + 1,
+                self.positions[tail_idx].1 - 1,
+            );
+        } else {
+            self.positions[tail_idx] = (
+                self.positions[tail_idx].0 - 1,
+                self.positions[tail_idx].1 - 1,
+            );
         }
     }
 }
@@ -96,25 +128,40 @@ fn are_neighbours(pos1: &Pos, pos2: &Pos) -> bool {
     pos1.0 - 1 <= pos2.0 && pos2.0 <= pos1.0 + 1 && pos1.1 - 1 <= pos2.1 && pos2.1 <= pos1.1 + 1
 }
 
-fn part1(moves: &[Move], rope: &mut Rope) -> usize {
-    rope.tail_positions.insert((0, 0));
+fn part1(moves: &[Move]) -> usize {
+    let mut rope = Rope {
+        positions: vec![(0, 0), (0, 0)],
+        visited: BTreeSet::new(),
+    };
+    rope.visited.insert((0, 0));
 
     for mv in moves {
         rope.handle_move(&mv);
     }
 
-    rope.tail_positions.len()
+    rope.visited.len()
+}
+
+fn part2(moves: &[Move]) -> usize {
+    let mut rope = Rope {
+        positions: [(0, 0)].repeat(10),
+        visited: BTreeSet::new(),
+    };
+    rope.visited.insert((0, 0));
+
+    for mv in moves {
+        rope.handle_move(&mv);
+    }
+
+    rope.visited.len()
 }
 
 fn main() {
     let moves = parse_moves("./data/input.txt");
 
-    let mut rope = Rope {
-        head: (0, 0),
-        tail: (0, 0),
-        tail_positions: BTreeSet::new(),
-    };
-
-    let part1_res = part1(&moves, &mut rope);
+    let part1_res = part1(&moves);
     println!("{:#?}", part1_res);
+
+    let part2_res = part2(&moves);
+    println!("{:#?}", part2_res);
 }
--- rs/q_9/src/tests.rs
+++ rs/q_9/src/tests.rs
@@ -3,13 +3,20 @@ use super::*;
 #[test]
 fn part1_works() {
     let moves = parse_moves("./data/demo.txt");
-
-    let mut rope = Rope {
-        head: (0, 0),
-        tail: (0, 0),
-        tail_positions: BTreeSet::new(),
-    };
-
-    let res = part1(&moves, &mut rope);
+    let res = part1(&moves);
     assert_eq!(res, 13);
+}
+
+#[test]
+fn part2_works() {
+    let moves = parse_moves("./data/demo.txt");
+    let res = part2(&moves);
+    assert_eq!(res, 1);
+}
+
+#[test]
+fn part2_advanced_works() {
+    let moves = parse_moves("./data/demo2.txt");
+    let res = part2(&moves);
+    assert_eq!(res, 36);
 }
