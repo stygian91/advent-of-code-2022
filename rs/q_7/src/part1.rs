5023c6d0a1166cde8b2052909e0a2467f77624fd
stygian91 <kioravuser@gmail.com>
2022-12-10 14:15:53 +0200

    Day 7 Rust - part 1 complete.

--- rs/q_7/src/part1.rs
+++ rs/q_7/src/part1.rs
@@ -1,11 +1,21 @@
+use super::common::*;
 
+const MAX_SIZE: usize = 100000;
 
-mod tests {
-    use super::*;
+pub fn part1(cwd: RcFile) -> usize {
+    let mut accumulator = 0;
 
-    #[test]
-    #[ignore]
-    fn part1_works() {
+    walk(cwd.clone(), &mut |file| {
+        let f_borrow = file.borrow();
+        if !f_borrow.is_dir {
+            return;
+        }
 
-    }
+        let size = f_borrow.size.unwrap();
+        if size <= MAX_SIZE {
+            accumulator += size;
+        }
+    });
+
+    accumulator
 }
