bb9be378294725ce1511bc33abaa745024d0a65f
stygian91 <kioravuser@gmail.com>
2022-12-10 14:23:41 +0200

    Day 7 Rust - part 2 complete.

--- rs/q_7/src/main.rs
+++ rs/q_7/src/main.rs
@@ -2,11 +2,14 @@
 
 mod common;
 mod part1;
+mod part2;
 
 use std::{rc::Rc, cell::RefCell, fs::read_to_string, path::Path};
 
 use common::*;
 use part1::part1;
+
+use crate::part2::part2;
 
 fn main() {
     let contents = read_to_string(&Path::new("./data/input.txt")).unwrap();
@@ -16,5 +19,8 @@ fn main() {
     parse(root.clone(), 1, &lines);
     update_dir_sizes(root.clone());
     let part1_res = part1(root.clone());
-    println!("{:#?}", part1_res);
+    println!("part 1: {:#?}", part1_res);
+
+    let part2_res = part2(root);
+    println!("part 2: {:#?}", part2_res);
 }
