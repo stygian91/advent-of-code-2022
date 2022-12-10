d2f09a263886a9f59c84b7db94ae34772e21aea2
stygian91 <kioravuser@gmail.com>
2022-12-10 13:37:28 +0200

    Day 7 Rust - implement file parsing.

--- rs/q_7/src/main.rs
+++ rs/q_7/src/main.rs
@@ -3,10 +3,16 @@
 mod common;
 mod part1;
 
-use std::{rc::Rc, cell::RefCell};
+use std::{rc::Rc, cell::RefCell, fs::read_to_string, path::Path};
 
 use common::*;
 
 fn main() {
+    let contents = read_to_string(&Path::new("./data/demo.txt")).unwrap();
+    let lines = contents.lines().collect::<Vec<&str>>();
 
+    let mut root = File::new_rc(File::new("/", true));
+    parse(root.clone(), 1, &lines);
+    let file_tree = root.borrow().print("-");
+    println!("{}", file_tree);
 }
