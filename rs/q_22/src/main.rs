e0508a402648ea6e000388f461decc66bac01ad3
stygian91 <kioravuser@gmail.com>
2022-12-30 15:13:34 +0200

    Day 22 - WIP

--- rs/q_22/src/main.rs
+++ rs/q_22/src/main.rs
@@ -1,7 +1,16 @@
+// use std::fs::read_to_string;
+
+// use bis::process_part1;
 use part1::part1;
 
+mod board;
 mod part1;
+
+// mod bis;
 
 fn main() {
     part1();
+    // let input = read_to_string("./data/input.txt").unwrap();
+    // let res = process_part1(&input);
+    // println!("{}", res);
 }
