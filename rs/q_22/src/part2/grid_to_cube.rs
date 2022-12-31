f713c8c4e29d7db80e39dd2f5dba5c3aa1841ea2
Stygian <kioravuser@gmail.com>
2022-12-31 21:57:56 +0200

    Day 22 - part 2 WIP

--- rs/q_22/src/part2/grid_to_cube.rs
+++ rs/q_22/src/part2/grid_to_cube.rs
@@ -1,7 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
-use super::cube::FaceIdentity::*;
+use super::cube::{FaceIdentity::*, AxisDirection};
 use super::cube::{Cube, Face, FaceIdentity};
+use super::cube::AxisDirection::*;
 
 fn transform(face_size: usize, grid: &Vec<Vec<char>>) -> Cube {
     let mut faces: HashMap<FaceIdentity, Face> = HashMap::new();
@@ -23,17 +24,30 @@ fn transform(face_size: usize, grid: &Vec<Vec<char>>) -> Cube {
     };
     faces.insert(Front, face);
 
-    let mut visited = HashSet::new();
+    let mut visited = HashMap::new();
     let mut to_visit = HashSet::new();
 
-    visited.insert(&face_coords[0]);
+    visited.insert(&face_coords[0], (Front, 0usize));
     for coord in face_coords.iter().skip(1) {
         to_visit.insert(coord);
     }
 
     while !to_visit.is_empty() {
+        let mut found = false;
+
         for &candidate in to_visit.iter() {
-            
+            if found {
+                break;
+            }
+
+            for (visited_coord, (ident, rots)) in visited.iter() {
+                if are_neighbours(candidate, visited_coord) {
+                    let new_visited = (Back, 0);
+                    visited.insert(candidate, new_visited);
+                    found = true;
+                    break;
+                }
+            }
         }
     }
 
@@ -60,6 +74,45 @@ fn get_grid_range(pos: &(usize, usize), face_size: usize, grid: &Vec<Vec<char>>)
 
     res
 }
+
+// assumes pos and pos2 are neighbours
+fn determine_direction(pos: &(usize, usize), pos2: &(usize, usize)) -> Option<AxisDirection> {
+    if pos2.0 > pos.0 {
+        return Some(XPositive);
+    }
+
+    if pos2.0 < pos.0 {
+        return Some(XNegative);
+    }
+
+    if pos2.1 > pos.1 {
+        return Some(YPositive);
+    }
+
+    if pos2.1 < pos.1 {
+        return Some(YNegative);
+    }
+
+    None
+}
+
+fn rotate_direction(dir: &AxisDirection, rots: usize) -> AxisDirection {
+    let adjusted_rots = rots % 4;
+
+    let dirs = [YNegative, XPositive, YPositive, XNegative];
+    let dir_idx = dirs.iter().position(|curr| *curr == *dir).unwrap();
+    let dir_idx = (dir_idx + adjusted_rots) % 4;
+
+    dirs[dir_idx].clone()
+}
+
+// fn rotate_range_once(range: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+
+// }
+
+// fn rotate_range(range: &Vec<Vec<char>>, rots: usize) -> Vec<Vec<char>> {
+//     let mut temp_range = range.clone();
+// }
 
 #[cfg(test)]
 mod tests {
@@ -94,5 +147,23 @@ mod tests {
         assert_eq!(are_neighbours(&(2,1), &(2,0)), true);
         assert_eq!(are_neighbours(&(1,2), &(1,3)), true);
         assert_eq!(are_neighbours(&(4,5), &(5,5)), true);
+    }
+
+    #[test]
+    fn determine_direction_works() {
+        assert_eq!(determine_direction(&(1,1), &(2,1)).unwrap(), XPositive);
+        assert_eq!(determine_direction(&(1,1), &(0,1)).unwrap(), XNegative);
+        assert_eq!(determine_direction(&(1,1), &(1,2)).unwrap(), YPositive);
+        assert_eq!(determine_direction(&(1,1), &(1,0)).unwrap(), YNegative);
+        assert!(determine_direction(&(1,1), &(1,1)).is_none());
+    }
+
+    #[test]
+    fn rotate_direction_works() {
+        assert_eq!(rotate_direction(&YNegative, 0), YNegative);
+        assert_eq!(rotate_direction(&YNegative, 1), XPositive);
+        assert_eq!(rotate_direction(&YNegative, 2), YPositive);
+        assert_eq!(rotate_direction(&YNegative, 3), XNegative);
+        assert_eq!(rotate_direction(&YNegative, 4), YNegative);
     }
 }
